67a0718c632ce46fc223a281c12647e0dd62b407
Matan Lurey <matan@lurey.org>
2025-07-14 22:34:43 -0700

    Add simple test

--- src/lib.rs
+++ src/lib.rs
@@ -30,3 +30,26 @@ impl<F: Format, T: GridWrite<Element = Color<F>>> DrawTarget<F> for PixelBuf<T, 
         let _ = self.inner.set(pos, color);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use grixy::{buf::ArrayGrid, core::RowMajor};
+
+    use super::*;
+
+    #[test]
+    fn test_draw_pixel() {
+        let grid = ArrayGrid::<Color<Rgba8888>, 1, RowMajor>::new_filled(
+            1,
+            1,
+            Color::<Rgba8888>::new(0xFF00_0000),
+        );
+        let mut pixel_buf = PixelBuf::new(grid);
+        pixel_buf.draw_pixel(Pos::new(0, 0), Color::<Rgba8888>::new(0xFFFF_FFFF));
+
+        assert_eq!(
+            pixel_buf.inner.get(Pos::new(0, 0)),
+            Some(&Color::<Rgba8888>::new(0xFFFF_FFFF))
+        );
+    }
+}
